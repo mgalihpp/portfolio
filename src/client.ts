0b10d0a676ea3361084f19bd29ba068acb500576
mgalihpp <muhammadgalih451@gmail.com>
2024-08-11 21:11:48 +0700

    add api version

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,7 @@ const client = createClient({
   projectId: 'g4pufrpg', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
   useCdn: true,
+  apiVersion: '2021-08-31',
 });
 
 export default client;
